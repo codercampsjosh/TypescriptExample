a2c3c270e93240bb14a65666350032390e50014f
Joshua <joshua.christiansen@codercamps.com>
2016-09-26 14:23:55 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var dogName = "Havoc";
+var dogAge = 2.5;
+console.log(dogName + " is " + dogAge + " years old");
---
+++ app.ts
@@ -0,0 +1,4 @@
+let dogName = `Havoc`;
+let dogAge = 2.5;
+
+console.log(`${dogName} is ${dogAge} years old`);
